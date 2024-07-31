f2fdbbd57f83dfa7761c61370bb72e9768c005c6
nricom <nricom@eafit.edu.co>
2024-07-31 18:29:42 -0500

    Visualizacion de peliculas con estilo

--- moviereviews/movie/models.py
+++ moviereviews/movie/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Movie(models.Model):
-    titel = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images')
     url = models.URLField(blank=True)
