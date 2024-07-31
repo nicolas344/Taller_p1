e32f68e0c6790e8684407840d3613f228d3fdc86
nricom <nricom@eafit.edu.co>
2024-07-31 18:11:26 -0500

    Visualizacion de peliculas con estilo

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import Movie
 
 def home(request):
     #return HttpResponse('<h1> Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html',{'name':'Nicolas Rico'})
+    #return render(request, 'home.html',{'name':'Nicolas Rico'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
 def about(request):
-    return render(request, 'about.html',{'name':'Nicolas Rico'})
+    return HttpResponse('<h1>Welcome to About Page</h1>')
